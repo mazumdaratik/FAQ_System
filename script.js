d851fa367fe362276c3f9aab3d3bb86c20cd8858
mazumdaratik <rjatik990@gmail.com>
2025-05-10 01:33:22 +0600

    FAQ System using JavaScript

---
+++ script.js
@@ -0,0 +1,18 @@
+const faq = document.querySelector(".faq");
+
+faq.addEventListener("click", function (e) {
+  if (e.target.classList.contains("question")) {
+    e.stopPropagation();
+
+    const currentItem = e.target.parentElement;
+    const currentAnswer = currentItem.querySelector(".answer");
+
+    currentAnswer.classList.toggle("show");
+  }
+});
+
+document.addEventListener("click", function (e) {
+  const allAnswer = document.querySelectorAll(".answer.show");
+
+  allAnswer.forEach((answer) => answer.classList.remove("show"));
+});
